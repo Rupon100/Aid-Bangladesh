c9f845c176daaa47b25ec919fa409ef28d7dc6b7
Rupon <fardiidlamrupon@gmail.com>
2024-09-23 12:59:54 +0600

    value calculation complete

--- js/main.js
+++ js/main.js
@@ -23,3 +23,93 @@ document.getElementById('donation-btn').addEventListener('click', ()=> {
     document.getElementById('history-btn').classList.toggle('bg-lime-300');
     document.getElementById('donation-btn').classList.toggle('bg-lime-300');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function for donate amount
+function donateValue(id){
+    const value = document.getElementById(id).textContent;
+    return parseFloat(value);
+}
+const mainBalance = donateValue('main-balance');
+const noakhaliDonate = donateValue('noakhali-donate');
+const feniDonate = donateValue('feni-donate');
+const studentAid = donateValue('student-aid');
+
+//function for input
+function inputValue(id){
+    const input = document.getElementById(id).value;
+    return parseFloat(input);
+}
+ 
+
+// const noakhaliBtn = document.getElementById('noakhali-btn');
+// noakhaliBtn.addEventListener('click', ()=> {
+//     const noakhaliInput = inputValue('noakhali-input');
+//     const mainBalance = donateValue('main-balance');
+//     const noakhaliDonate = donateValue('noakhali-donate');
+//     const ttlBalance = noakhaliInput + mainBalance;
+
+//     if(isNaN(noakhaliInput)){
+//         alert('Invalid Input!');
+//         document.getElementById('noakhali-input').value = '';
+//         return;
+//     }else if(noakhaliInput <= 0){
+//         alert('Invalid Input!');
+//         document.getElementById('noakhali-input').value = '';
+//         return;
+//     }
+//     else{
+//         const ttlForBox = noakhaliInput + noakhaliDonate;
+//         document.getElementById('noakhali-donate').innerText = ttlForBox;
+//         document.getElementById('main-balance').innerText = ttlBalance;
+//         document.getElementById('noakhali-input').value = '';
+
+//         const modal = document.getElementById('modal');
+//         noakhaliBtn.onclick = () => {
+//             modal.showModal();
+//         }
+//     }
+    
+// });
+
+//const noakhaliBtn = document.getElementById('noakhali-btn');
+ 
+
+//function for input
+function inputCalculate(inputId, mainbalanceId, donateId){
+    const input = inputValue(inputId);
+    const mainBalance = donateValue(mainbalanceId);
+    const donate = donateValue(donateId);
+
+    if(isNaN(input)){
+        alert('Invalid Input!');
+        document.getElementById(inputId).value = '';
+        return;
+    }else if(input <= 0){
+        alert('Invalid Input!');
+        document.getElementById(inputId).value = '';
+        return;
+    }else {
+        const ttlDoante = input + donate;
+        const updateBalance = mainBalance + input;
+
+        document.getElementById(donateId).innerText = ttlDoante;
+        document.getElementById(mainbalanceId).innerText = updateBalance;
+
+        document.getElementById(inputId).value = '';
+    }
+   
+}
