e7ba460cbecbf1905fab44cd2bc4a1d251a163ff
Rupon <fardiidlamrupon@gmail.com>
2024-09-23 23:17:40 +0600

    main balance calculation fixed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,33 @@
+
+
 //scroll sticky style added for header
 window.addEventListener('scroll', ()=>{
     const header = document.getElementById('scrollHeader');
     const nav = document.getElementById('navScroll');
-    if(window.scrollY > 50){
-        header.classList.add('bg-gray/70', 'backdrop-blur-xl')
-        //nav.classList.remove('bg-stone-100');
-    } 
+    if(window.scrollY > 40){
+        nav.classList.remove('bg-stone-100');
+        header.classList.add('bg-gray/60', 'backdrop-blur-xl') 
+    } else {
+        nav.classList.add('bg-stone-100');
+    }
 })
+
 
 //toggle for donation and history
 document.getElementById('history-btn').addEventListener('click', ()=> {
-    document.getElementById('donation-post').classList.toggle('hidden');
-    document.getElementById('history-post').classList.toggle('hidden');
+    document.getElementById('donation-post').classList.add('hidden');
+    document.getElementById('history-post').classList.remove('hidden');
 
-    document.getElementById('history-btn').classList.toggle('bg-lime-300');
-    document.getElementById('donation-btn').classList.toggle('bg-lime-300');
+    document.getElementById('history-btn').classList.add('bg-lime-300','hover:bg-lime-300');
+    document.getElementById('donation-btn').classList.remove('bg-lime-300', 'hover:bg-lime-300');
 });
 document.getElementById('donation-btn').addEventListener('click', ()=> {
-    document.getElementById('history-post').classList.toggle('hidden');
-    document.getElementById('donation-post').classList.toggle('hidden');
+    document.getElementById('history-post').classList.add('hidden');
+    document.getElementById('donation-post').classList.remove('hidden');
 
-    document.getElementById('history-btn').classList.toggle('bg-lime-300');
-    document.getElementById('donation-btn').classList.toggle('bg-lime-300');
+    document.getElementById('history-btn').classList.remove('bg-lime-300', 'hover:bg-lime-300');
+    document.getElementById('donation-btn').classList.add('bg-lime-300', 'hover:bg-lime-300');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //function for donate amount
@@ -43,10 +35,10 @@ function donateValue(id){
     const value = document.getElementById(id).textContent;
     return parseFloat(value);
 }
-const mainBalance = donateValue('main-balance');
-const noakhaliDonate = donateValue('noakhali-donate');
-const feniDonate = donateValue('feni-donate');
-const studentAid = donateValue('student-aid');
+// const mainBalance = donateValue('main-balance');
+// const noakhaliDonate = donateValue('noakhali-donate');
+// const feniDonate = donateValue('feni-donate');
+// const studentAid = donateValue('student-aid');
 
 //function for input
 function inputValue(id){
@@ -61,14 +53,14 @@ function inputCalculate(inputId, mainbalanceId, donateId, place, modalId){
     const mainBalance = donateValue(mainbalanceId);
     const donate = donateValue(donateId);
 
-    if(isNaN(input) || input <= 0){
+    if(isNaN(input) || input > mainBalance || input < 0){
         console.log('holada')
-        alert('Invalid Input!');
+        alert('Invalid Donation Amount!');
         document.getElementById(inputId).value = '';
         return;
     } else{
         const ttlDoante = input + donate;
-        const updateBalance = mainBalance + input;
+        const updateBalance = mainBalance - input;
         document.getElementById(donateId).innerText = ttlDoante;
         document.getElementById(mainbalanceId).innerText = updateBalance;
         document.getElementById(inputId).value = '';
@@ -85,7 +77,8 @@ function inputCalculate(inputId, mainbalanceId, donateId, place, modalId){
     div.innerHTML = `
         <div class="border rounded-lg p-6">
             <h1 class="text-xl font font-semibold py-1">${input} Taka is Donated for ${floodPlace}</h1>
-            <p>Date: ${date.toLocaleDateString()} ${date.toLocaleTimeString()}</p>
+            <p>Date: ${date.toLocaleDateString()} ${date.toLocaleTimeString()} (Bangladesh Standerd Time)</p>
+            
         </div>
     `;
     historyBox.appendChild(div);
